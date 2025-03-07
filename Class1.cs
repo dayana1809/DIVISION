d7d033857c89f1a7e5d56d5f0054e6eadb6b7800
Usuario <Usuario@B6-603-37795>
2025-03-07 13:42:25 -0500

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIVISION
+{
+    public class Class1
+    {
+        /// <div>
+        /// 
+        /// </div>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static long Division(long a, long b)
+        {
+            return (a / b);
+        }
+    }
+}
